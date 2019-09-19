113cfff0c57a3086d626f3ae64a5d82c33adaa1d
Matheus Souza <matheus.souzadv@gmail.com>
2019-09-19 17:30:23 -0300

    CardConfig

--- src/routes.js
+++ src/routes.js
@@ -36,7 +36,7 @@ export const AppNavigator = createStackNavigator(
     transparentCard: true,
     headerMode: 'none',
     mode: 'modal',
-    initialRouteName: 'CardConfig',
+    initialRouteName: 'App',
     transitionConfig: () => ({
       transitionSpec: {
         duration: 750,
